77c02d7ebc6853879f19684e2fa1fb5fccde614c
Don Isaac <donald.isaac@gmail.com>
2024-11-15 13:28:13 -0500

    test: add options parsing test cases

--- src/cli/Options.zig
+++ src/cli/Options.zig
@@ -9,9 +9,18 @@ print_ast: bool = false,
 /// Positional arguments
 args: std.ArrayListUnmanaged(util.string) = .{},
 
-pub fn parseArgv(alloc: std.mem.Allocator) Options {
+const ParseError = error{OutOfMemory};
+pub fn parseArgv(alloc: Allocator) ParseError!Options {
+    // NOTE: args() is not supported on WASM and windows. When targeting another
+    // platform, argsWithAllocator does not actually allocate memory.
+    var argv = try std.process.argsWithAllocator(alloc);
+    defer argv.deinit();
+    return parse(alloc, argv);
+}
+
+fn parse(alloc: Allocator, args_iter: anytype) ParseError!Options {
     var opts = Options{};
-    var argv = std.process.args();
+    var argv = args_iter;
 
     // skip binary name
     _ = argv.next() orelse {
@@ -41,10 +50,59 @@ pub fn deinit(self: *Options, alloc: std.mem.Allocator) void {
     self.args.deinit(alloc);
 }
 
-inline fn eq(arg: [:0]const u8, name: [:0]const u8) bool {
+inline fn eq(arg: anytype, name: @TypeOf(arg)) bool {
     return std.mem.eql(u8, arg, name);
 }
+// inline fn eq(arg: [:0]const u8, name: [:0]const u8) bool {
+//     return std.mem.eql(u8, arg, name);
+// }
 
 const Options = @This();
 const std = @import("std");
 const util = @import("util");
+const Allocator = std.mem.Allocator;
+
+test parse {
+    const t = std.testing;
+    const List = std.ArrayListUnmanaged(util.string);
+    const Case = std.meta.Tuple(&[_]type{ []const u8, Options });
+
+    const src_list: List = brk: {
+        const items = &[_]util.string{"src"};
+        break :brk List{ .items = @constCast(items) };
+    };
+    const src_test_list: List = brk: {
+        const items = &[_]util.string{ "src", "test" };
+        break :brk List{ .items = @constCast(items) };
+    };
+
+    const test_cases = [_]Case{
+        .{ "", .{} },
+        .{ "zlint", .{} },
+        .{ "zlint --", .{} },
+        .{ "zlint --verbose", .{ .verbose = true } },
+        .{ "zlint -V", .{ .verbose = true } },
+        .{ "zlint src -V", .{ .verbose = true, .args = src_list } },
+        .{ "zlint -V src", .{ .verbose = true, .args = src_list } },
+        .{ "zlint -V -- src", .{ .verbose = true, .args = src_list } },
+        .{ "zlint -V src test ", .{ .verbose = true, .args = src_test_list } },
+        .{ "zlint -V -- src test ", .{ .verbose = true, .args = src_test_list } },
+        .{ "zlint src -V test", .{ .verbose = true, .args = src_test_list } },
+    };
+
+    for (test_cases) |test_case| {
+        const argv = std.mem.splitScalar(u8, test_case[0], ' ');
+        const expected: Options = test_case[1];
+        const opts = try parse(std.heap.page_allocator, argv);
+
+        try t.expectEqual(expected.verbose, opts.verbose);
+        try t.expectEqual(expected.print_ast, opts.print_ast);
+        try t.expectEqual(expected.args.items.len, opts.args.items.len);
+        for (0..expected.args.items.len) |i| {
+            try t.expectEqualStrings(
+                expected.args.items[i],
+                opts.args.items[i],
+            );
+        }
+    }
+}
