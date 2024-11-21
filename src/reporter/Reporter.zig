065b5e952d5d5ccdd4fc785c8957248076ad19b8
Don Isaac <donald.isaac@gmail.com>
2024-11-21 16:02:51 -0500

    feat(linter): print summary statistics after linting

--- src/reporter/Reporter.zig
+++ src/reporter/Reporter.zig
@@ -1,4 +1,5 @@
 pub const FormatError = Writer.Error || std.mem.Allocator.Error;
+const Chameleon = @import("chameleon");
 
 pub fn Reporter(
     Formatter: type,
@@ -8,6 +9,7 @@ pub fn Reporter(
         writer: Writer,
         writer_lock: Mutex = .{},
         formatter: Formatter,
+        stats: Stats = .{},
 
         const Self = @This();
         pub fn init(writer: Writer, formatter: Formatter) Self {
@@ -16,7 +18,10 @@ pub fn Reporter(
                 .formatter = formatter,
             };
         }
+
         pub fn reportErrors(self: *Self, errors: std.ArrayList(Error)) void {
+            self.stats.recordErrors(errors.items.len);
+            defer errors.deinit();
             if (errors.items.len == 0) return;
             self.writer_lock.lock();
             defer self.writer_lock.unlock();
@@ -27,13 +32,58 @@ pub fn Reporter(
                 self.writer.writeByte('\n') catch @panic("failed to write newline.");
                 e.deinit(errors.allocator);
             }
-            errors.deinit();
+        }
+        pub fn printStats(self: *Self, duration: i64) void {
+            const yellow, const yd = comptime blk: {
+                var c = Chameleon.initComptime();
+                const yellow = c.yellow().createPreset();
+                const yd = yellow.open ++ "{d}" ++ yellow.close;
+                // const yk = yellow.open ++ "{d}" ++ yellow.close;
+                break :blk .{ yellow, yd };
+            };
+
+            const errors = self.stats.numErrorsSync();
+            const files = self.stats.numFilesSync();
+            // yellow.fmt()
+            self.writer.print(
+                // "\tFound {s} errors across {s} files in {s}ms.\n",
+                "\tFound " ++ yd ++ " errors across " ++ yd ++ " files in " ++ yellow.open ++ "{d}ms" ++ yellow.close ++ ".\n",
+                .{ errors, files, duration },
+            ) catch {};
         }
     };
 }
 
+const Stats = struct {
+    num_files: AtomicUsize = AtomicUsize.init(0),
+    num_errors: AtomicUsize = AtomicUsize.init(0),
+
+    pub fn recordErrors(self: *Stats, num_errors: usize) void {
+        _ = self.num_files.fetchAdd(1, .acquire);
+        _ = self.num_errors.fetchAdd(num_errors, .acquire);
+    }
+
+    pub fn recordSuccess(self: *Stats) void {
+        _ = self.num_files.fetchAdd(1, .acquire);
+    }
+
+    /// Get the number of linted files. Only call this after all files have been
+    /// processsed.
+    pub fn numFilesSync(self: *const Stats) usize {
+        return self.num_files.raw;
+    }
+
+    /// Get the number of lint errors. Only call this after all files have been
+    /// processsed.
+    pub fn numErrorsSync(self: *const Stats) usize {
+        return self.num_errors.raw;
+    }
+};
+
 const std = @import("std");
 const Error = @import("../Error.zig");
 const Span = @import("../source.zig").Span;
+
+const AtomicUsize = std.atomic.Value(usize);
 const Mutex = std.Thread.Mutex;
 const Writer = std.fs.File.Writer; // TODO: use std.io.Writer?
