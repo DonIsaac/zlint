d58c14c69f1507f4284e99e3d5c3bd72f3c5a37f
Don Isaac <donald.isaac@gmail.com>
2024-11-22 15:41:33 -0500

    feat(semantic): link scopes to the nodes that create them

--- src/linter/rules.zig
+++ src/linter/rules.zig
@@ -1,2 +1,3 @@
 pub const NoUndefined = @import("./rules/no_undefined.zig");
 pub const NoUnresolved = @import("./rules/no_unresolved.zig");
+pub const HomelessTry = @import("./rules/homeless_try.zig");
