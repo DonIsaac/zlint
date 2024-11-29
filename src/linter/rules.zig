c29684611fcef4892bd92b0234de2f96b94ce2b9
Don Isaac <donald.isaac@gmail.com>
2024-11-29 15:48:50 -0500

    feat(linter): add `unused-decls` rule (#144)

--- src/linter/rules.zig
+++ src/linter/rules.zig
@@ -2,3 +2,4 @@ pub const HomelessTry = @import("./rules/homeless_try.zig");
 pub const NoCatchReturn = @import("./rules/no_catch_return.zig");
 pub const NoUndefined = @import("./rules/no_undefined.zig");
 pub const NoUnresolved = @import("./rules/no_unresolved.zig");
+pub const UnusedDecls = @import("./rules/unused_decls.zig");
