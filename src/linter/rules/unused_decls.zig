d98d8fbfcabaab1ae66241a5caf604f98b6bfb31
Don Isaac <donald.isaac@gmail.com>
2024-11-29 22:00:43 -0500

    feat(linter): add `suppressed-errors` rule

--- src/linter/rules/unused_decls.zig
+++ src/linter/rules/unused_decls.zig
@@ -1,13 +1,13 @@
 //! ## What This Rule Does
-//! 
+//!
 //! Disallows container-scoped variables that are declared but never used. Note
 //! that top-level declarations are included.
-//! 
+//!
 //! The Zig compiler checks for unused parameters, payloads bound by `if`,
 //! `catch`, etc, and `const`/`var` declaration within functions. However,
 //! variables and functions declared in container scopes are not given the same
 //! treatment. This rule handles those cases.
-//! 
+//!
 //! > [!WARNING]
 //! > ZLint's semantic analyzer does not yet record references to variables on
 //! > member access expressions (e.g. `bar` on `foo.bar`). It also does not
@@ -21,12 +21,12 @@
 //! // `std` used, but `Allocator` is not.
 //! const std = @import("std");
 //! const Allocator = std.mem.Allocator;
-//! 
+//!
 //! // Variables available to other code, either via `export` or `pub`, are not
 //! // reported.
 //! pub const x = 1;
 //! export fn foo(x: u32) void {}
-//! 
+//!
 //! // `extern` functions are not reported
 //! extern fn bar(a: i32) void;
 //! ```
@@ -36,7 +36,7 @@
 //! // Discarded variables are considered "used".
 //! const x = 1;
 //! _ = x;
-//! 
+//!
 //! // non-container scoped variables are allowed by this rule but banned by the
 //! // compiler. `x`, `y`, and `z` are ignored by this rule.
 //! pub fn foo(x: u32) void {
