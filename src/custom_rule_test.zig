71f287bc1f1726a955d0f1f372db2e20aacf585a
Michael Belousov <michael.belousov98@gmail.com>
2024-12-28 22:24:29 -0500

    stupid impl for test rule

--- src/custom_rule_test.zig
+++ src/custom_rule_test.zig
@@ -12,11 +12,32 @@ pub const meta: Rule.Meta = .{
 };
 
 pub fn runOnNode(self: *const anyopaque, node: NodeWrapper, ctx: *LinterContext) !void {
-    std.debug.print("{*},{any},{*}", .{ self, node, ctx });
+    //std.debug.print("{*},{any},{*}", .{ self, node, ctx });
+    _ = self;
+    _ = node;
+    _ = ctx;
 }
 
 pub fn runOnSymbol(self: *const anyopaque, sym: Symbol.Id, ctx: *LinterContext) !void {
-    std.debug.print("{*},{},{*}", .{ self, sym, ctx });
+    _ = self;
+    const s = sym.into(usize);
+    const slice = ctx.symbols().symbols.slice();
+    const flags: Symbol.Flags = slice.items(.flags)[s];
+    if (!(flags.s_variable and flags.s_const))
+        return;
+
+    const name = slice.items(.name)[s];
+
+    const e = ctx.diagnosticFmt(
+        "hello from user defined rule! symbol: '{s}'",
+        .{name},
+        .{ctx.spanT(slice.items(.token)[s].unwrap().?.int())},
+    );
+
+    e.help = .{
+        .str = "how fun",
+        .static = true,
+    };
 }
 
 // FIXME: the runtime compiler invocation will have its own entry zig file which imports all the above
