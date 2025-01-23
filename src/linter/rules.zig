421f7a233f3bdcae4c94b9754192a6fa1a77f3be
Don Isaac <donald.isaac@gmail.com>
2025-01-23 00:36:59 -0800

    chore: sort rules

--- src/linter/rules.zig
+++ src/linter/rules.zig
@@ -1,7 +1,7 @@
 pub const HomelessTry = @import("./rules/homeless_try.zig");
 pub const NoCatchReturn = @import("./rules/no_catch_return.zig");
-pub const UnsafeUndefined = @import("./rules/unsafe_undefined.zig");
-pub const NoUnresolved = @import("./rules/no_unresolved.zig");
-pub const UnusedDecls = @import("./rules/unused_decls.zig");
-pub const SuppressedErrors = @import("./rules/suppressed_errors.zig");
 pub const NoReturnTry = @import("./rules/no_return_try.zig");
+pub const NoUnresolved = @import("./rules/no_unresolved.zig");
+pub const SuppressedErrors = @import("./rules/suppressed_errors.zig");
+pub const UnsafeUndefined = @import("./rules/unsafe_undefined.zig");
+pub const UnusedDecls = @import("./rules/unused_decls.zig");
