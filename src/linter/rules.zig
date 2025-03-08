93b17f7eb347a6e518e6ceb4a7e5c45ce93b36b9
Felipe Simões <34526320+felipeasimos@users.noreply.github.com>
2025-03-08 07:29:48 -0300

    feat(linter): add `empty-file` rule (#245)
    Co-authored-by: Don Isaac <donald.isaac@gmail.com>

--- src/linter/rules.zig
+++ src/linter/rules.zig
@@ -8,3 +8,4 @@ pub const SuppressedErrors = @import("./rules/suppressed_errors.zig");
 pub const UnsafeUndefined = @import("./rules/unsafe_undefined.zig");
 pub const UnusedDecls = @import("./rules/unused_decls.zig");
 pub const UselessErrorReturn = @import("./rules/useless_error_return.zig");
+pub const EmptyFile = @import("./rules/empty_file.zig");
