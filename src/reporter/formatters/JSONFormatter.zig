ab57dcc1fa6d1973dbe6090fd2e727a2141629d1
Don Isaac <donald.isaac@gmail.com>
2025-04-05 18:27:28 -0700

    add tests

--- src/reporter/formatters/JSONFormatter.zig
+++ src/reporter/formatters/JSONFormatter.zig
@@ -16,7 +16,34 @@ pub fn format(_: *JSONFormatter, w: *Writer, e: Error) FormatError!void {
     return std.json.stringify(e, .{}, w.*);
 }
 
+test JSONFormatter {
+    const json = std.json;
+    const allocator = std.testing.allocator;
+    const expectEqualStrings = std.testing.expectEqualStrings;
+
+    var buf = std.ArrayList(u8).init(allocator);
+    defer buf.deinit();
+
+    var err = Error.newStatic("oof");
+    defer err.deinit(allocator);
+    err.help = Cow.static("help pls");
+    err.code = "code";
+
+    var f = JSONFormatter{};
+    var w = buf.writer().any();
+    try f.format(&w, err);
+
+    var value = try json.parseFromSlice(json.Value, allocator, buf.items, .{});
+    defer value.deinit();
+    const obj = value.value.object;
+
+    try expectEqualStrings("oof", obj.get("message").?.string);
+    try expectEqualStrings("code", obj.get("code").?.string);
+    try expectEqualStrings("help pls", obj.get("help").?.string);
+}
+
 const std = @import("std");
+const Cow = @import("util").Cow(false);
 const formatter = @import("../formatter.zig");
 const Meta = formatter.Meta;
 const FormatError = formatter.FormatError;
