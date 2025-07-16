fe9d0036bc141cc748742605ed69e62eb52111bf
Don Isaac <donald.isaac@gmail.com>
2025-07-15 18:39:30 -0700

    fix(case-convention): generics should use PascalCase

--- src/linter/rules/case_convention.zig
+++ src/linter/rules/case_convention.zig
@@ -1,24 +1,37 @@
 //! ## What This Rule Does
-//! Checks for function names that are not in camel case. Specially coming from Rust,
-//! some people may be used to use snake_case for their functions, which can lead to
-//! inconsistencies in the code
+//! Enforces Zig's naming convention.
+//!
+//! :::warning
+//! Only functions are checked at this time.
+//! :::
+//!
+//! ## Functions
+//! In general, functions that return values should use camelCase names while
+//! those that return types should use PascalCase. Specially coming from Rust,
+//! some people may be used to use snake_case for their functions, which can
+//! lead to inconsistencies in the code.
+//!
 //!
 //! ## Examples
 //!
 //! Examples of **incorrect** code for this rule:
 //! ```zig
 //! fn this_one_is_in_snake_case() void {}
+//! fn generic(T: type) T { return T{}; }
 //! ```
 //!
 //! Examples of **correct** code for this rule:
 //! ```zig
 //! fn thisFunctionIsInCamelCase() void {}
+//! fn Generic(T: type) T { return T{}; }
 //! ```
 
 const std = @import("std");
+const util = @import("util");
 const semantic = @import("../../semantic.zig");
 const _rule = @import("../rule.zig");
 const _span = @import("../../span.zig");
+const ast_utils = @import("../ast_utils.zig");
 
 const Ast = std.zig.Ast;
 const Node = Ast.Node;
@@ -82,36 +95,76 @@ fn getCase(string: []const u8) CaseType {
     return .camelCase;
 }
 
-pub fn functionNameDiagnostic(ctx: *LinterContext, fn_name: []const u8, case: CaseType, span: Span) Error {
+fn functionNameDiagnostic(ctx: *LinterContext, fn_name: []const u8, case: CaseType, name: Ast.TokenIndex) Error {
     if (case == .NotCamelCase) {
-        return ctx.diagnosticf("Function {s} name is not in camelCase", .{fn_name}, .{LabeledSpan{ .span = span }});
+        return ctx.diagnosticf(
+            "Function {s} name is not in camelCase",
+            .{fn_name},
+            .{ctx.spanT(name)},
+        );
     }
-    return ctx.diagnosticf("Function {s} name is in {s}. It should be camelCase", .{ fn_name, @tagName(case) }, .{LabeledSpan{ .span = span }});
+    return ctx.diagnosticf(
+        "Function {s} name is in {s}. It should be camelCase",
+        .{ fn_name, @tagName(case) },
+        .{ctx.spanT(name)},
+    );
+}
+fn genericsArePascaleCase(ctx: *LinterContext, fn_name: []const u8, name: Ast.TokenIndex) Error {
+    var d = ctx.diagnosticf(
+        "Function '{s}' returns a type, but does not use PascalCase",
+        .{fn_name},
+        .{ctx.spanT(name)},
+    );
+    d.help = .static("By convention, Zig uses PascalCase for structs, generics, and all other type variables.");
+    return d;
 }
 
 pub fn runOnSymbol(_: *const CaseConvention, symbol: Symbol.Id, ctx: *LinterContext) void {
-    const nodes = ctx.ast().nodes;
+    // const nodes = ctx.ast().nodes;
     const symbols = ctx.symbols().symbols.slice();
     const symbol_flags: []const Symbol.Flags = symbols.items(.flags);
     const id = symbol.into(usize);
 
-    const flags = symbol_flags[symbol.into(usize)];
+    const flags = symbol_flags[id];
     if (!flags.s_fn) return;
 
-    const tags: []const Node.Tag = nodes.items(.tag);
     const decl: Node.Index = symbols.items(.decl)[id];
-    const tag: Node.Tag = tags[decl];
-    if (tag != .fn_decl and tag != .fn_proto) return; // could be .fn_proto for e.g. fn types
+    // if (tag != .fn_decl and tag != .fn_proto) return; // could be .fn_proto for e.g. fn types
+    var buf: [1]Node.Index = undefined;
+    const func = ctx.ast().fullFnProto(&buf, decl) orelse {
+        util.debugAssert(false, "Symbol flagged as a function does not correspond to an fn-like node", .{});
+        return;
+    };
 
-    const fn_name = symbols.items(.name)[id];
+    const fn_name: []const u8 = symbols.items(.name)[id];
     const case = getCase(fn_name);
-    if (case != .camelCase) {
-        const ast = ctx.ast();
-        const fn_keyword_token_idx = ast.nodes.items(.main_token)[id];
-        const name_token_idx = fn_keyword_token_idx + 1;
-        const span = ast.tokenToSpan(name_token_idx);
-        ctx.report(functionNameDiagnostic(ctx, fn_name, case, .{ .start = span.start, .end = span.end }));
+    const returns_type = fnReturnsType(ctx, &func);
+    if (returns_type) {
+        if (case != .PascalCase) {
+            ctx.report(genericsArePascaleCase(ctx, fn_name, func.name_token.?));
+        }
+    } else {
+        if (case != .camelCase) {
+            const ast = ctx.ast();
+            const fn_keyword_token_idx: Ast.TokenIndex = ast.nodes.items(.main_token)[id];
+            const name_token_idx = fn_keyword_token_idx + 1;
+            ctx.report(functionNameDiagnostic(ctx, fn_name, case, name_token_idx));
+        }
     }
+}
+
+fn fnReturnsType(ctx: *LinterContext, fn_proto: *const Ast.full.FnProto) bool {
+    const return_type = ast_utils.getRightmostIdentifier(
+        ctx,
+        ast_utils.getInnerType(ctx.ast(), fn_proto.ast.return_type),
+    ) orelse {
+        return false;
+    };
+    if (return_type.len == 0) {
+        @branchHint(.cold);
+        return false;
+    }
+    return std.ascii.isUpper(return_type[0]) or std.mem.eql(u8, return_type, "type");
 }
 
 // Used by the Linter to register the rule so it can be run.
@@ -127,22 +180,9 @@ test CaseConvention {
     var runner = RuleTester.init(t.allocator, case_convention.rule());
     defer runner.deinit();
 
-    // Code your rule should pass on
-    const pass = &[_][:0]const u8{
-        // TODO: add test cases
-        "fn alllowercasefunctionsarealwaysgreen() void {}",
-        "fn thisFunctionIsInCamelCase() void {}",
-    };
+    const pass = &[_][:0]const u8{ "fn alllowercasefunctionsarealwaysgreen() void {}", "fn thisFunctionIsInCamelCase() void {}", "fn Generic(T: type) T { return T{}; }", "fn FooBar() type { return u32; }" };
 
-    // Code your rule should fail on
-    const fail = &[_][:0]const u8{
-        // TODO: add test cases
-        "fn ThisFunctionIsInPascalCase() void {}",
-        "fn @\"this-one-is-in-kebab-case\"() void {}",
-        "fn this_one_is_in_snake_case() void {}",
-        "fn @\"This-is-both-Pascal-and-Kebab-kinda\"() void {}",
-        "fn This_is_both_snake_case_and_pascal_kinda() void {}",
-    };
+    const fail = &[_][:0]const u8{ "fn ThisFunctionIsInPascalCase() void {}", "fn @\"this-one-is-in-kebab-case\"() void {}", "fn this_one_is_in_snake_case() void {}", "fn @\"This-is-both-Pascal-and-Kebab-kinda\"() void {}", "fn This_is_both_snake_case_and_pascal_kinda() void {}", "fn This_is_both_snake_case_and_pascal_kinda(a: u32, b: u32, c: u32, d: u32) void {}", "fn fooBar() type { return u32; }" };
 
     try runner
         .withPass(pass)
